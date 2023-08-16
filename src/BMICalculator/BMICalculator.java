d77806ca5da1c25f5b7e1ddd29dd7dfcfaaa4ee5
koketso Raseroka <89457901+KoketsoRaseroka@users.noreply.github.com>
2023-08-16 18:19:49 +0200

    Add files via upload

---
+++ src/BMICalculator/BMICalculator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BMICalculator;
+
+
+
+/**
+ *
+ * @author Koket
+ */
+public class BMICalculator {
+    
+    
+    public static void main (String [] args){
+        //calling the JFrame class to display content and componenets
+        BMICalculator1 Bmi = new BMICalculator1();
+        Bmi.show();
+    
+    }
+}
